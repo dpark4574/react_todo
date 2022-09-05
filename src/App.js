df7e45141282d6365d714ce1566e8ba0425bdd2a
Daniel Park <dpark4574@gmail.com>
2022-09-05 17:28:44 -0400

    event handlers added for todo lists

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text="CMSC330 Project 1" />
+      <Todo text="ENEE303 HW" />
+      <Todo text="ENEE303 Lecture Quiz 2" />
     </div>
   );
 }
